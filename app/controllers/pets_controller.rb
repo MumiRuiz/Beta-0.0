e37f90b94bce5217c66b8b410247cd85dcc3e293
pipemora <tarefasimple@gmail.com>
2014-09-26 15:16:24 -0500

    modelo listo de pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -69,6 +69,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:name, :description, :age, :breed, :type, :gender, :size, :special_reeds, :instituion_id)
+      params.require(:pet).permit(:name, :description, :age, :breed, :gender, :size, :special_reeds)
     end
 end
