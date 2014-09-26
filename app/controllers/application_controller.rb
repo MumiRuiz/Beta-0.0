30475f14ebd3659b2e2e638e67a92afa04a3ce72
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-09-25 21:34:24 -0500

    images upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:name, :location, :description, :email, :password) }
+    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:name, :country_id, :description, :email, :password) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725211901) do
+ActiveRecord::Schema.define(version: 20140926004831) do
 
   create_table "posts", force: true do |t|
     t.text     "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140725211901) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "country_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
